d0f5728bf39f831a2fe61d899edad0b55a481c63
Paul Shen <pxshen@alumni.stanford.edu>
2024-01-24 22:34:11 -0500

    a

--- src/main.jl
+++ src/main.jl
@@ -4,4 +4,4 @@ using LazyArrays
 include("types.jl")
 include("alg.jl")
 include("pad.jl")
-export pad, pad!, Ramp, ReplicateRamp, PaddedArray
+export pad, pad!, Ramp, ReplicateRamp, PaddedArray, left, right
